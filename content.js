f55b01c11491dbf7ee24ec147e4c5c49ca5d0e8a
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2022-11-07 05:43:31 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
+function changeKievToKyiv(from, to){ //We create a function that manages to obtain every reference to Kiev on the web page. 
+    
+getEveryTextNode().forEach(function(node))    
+    
+    }
 
+changeKievToKyiv('kiev', 'kyiv');
+
+changeKievToKyiv('Kiev', 'Kyiv');
