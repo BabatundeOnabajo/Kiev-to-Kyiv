caeee1c377248dec7665452aef4bc215d86bf822
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2022-11-07 05:49:31 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,20 @@
 function changeKievToKyiv(from, to){ //We create a function that manages to obtain every reference to Kiev on the web page. 
     
-getEveryTextNode().forEach(function(node))    
+getEveryTextNode().forEach(function(node){
     
-    }
+    node.nodeValue = node.nodeValue.replace(new RegExp(changeMaker(from), 'g'), to);
+    
+    });
+    
+    
+    
+function changeMaker(str){ //We use this regular expression to catch instances of "Kiev" so that we can change it to Kyiv.
+    return (str+' ').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+}
 
+}
+
+// We call the function changeKievToKyiv here.
 changeKievToKyiv('kiev', 'kyiv');
 
 changeKievToKyiv('Kiev', 'Kyiv');
